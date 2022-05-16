ac7cf3f4c744e4ec107c95b157c47bf81d01687a
StephArn <stefana.arina@gmail.com>
2022-05-16 19:18:31 +0300

    setup firebase + basic share

--- app/src/main/java/com/example/vacante/MainActivity.java
+++ app/src/main/java/com/example/vacante/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vacante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //if (savedInstanceState == null ) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed( true )
-                    .add(R.id.navbar, NavBar.class, null )
-                    .commit();
-        //}
+//        if (savedInstanceState == null ) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed( true )
+//                    .add(R.id.navbar, NavBar.class, null )
+//                    .commit();
+//        }
+
+        Button btn = (Button)findViewById(R.id.sharebttn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+            }
+        });
+
     }
 }
