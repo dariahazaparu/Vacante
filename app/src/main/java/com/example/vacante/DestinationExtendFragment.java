128eb88a1b8b1306cce9dd62055825f38898c019
StephArn <stefana.arina@gmail.com>
2022-05-18 11:50:40 +0300

    share destinations

--- app/src/main/java/com/example/vacante/DestinationExtendFragment.java
+++ app/src/main/java/com/example/vacante/DestinationExtendFragment.java
@@ -1,7 +1,9 @@
 package com.example.vacante;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,10 +28,24 @@ public class DestinationExtendFragment extends Fragment {
             Destination dest = bundle.getParcelable(DestinationFragment.DEST);
         }
 
+//        view.findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                createNotif();
+//            }
+//        });
+
         view.findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createNotif();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                assert bundle != null;
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Vreau sa merg in "+bundle.getString(DestinationFragment.DEST_NAME)+ "!");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
             }
         });
     }
